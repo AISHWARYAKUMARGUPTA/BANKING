49cff576714d9d6ff20d2fcb843482f83616fb03
amdjfsonb06 <amdjfsonb06@Amdsepjfs260.iiht.tech>
2022-09-22 20:43:33 +0530

    INITIAL COMMIT

---
+++ src/banking/bank.java
@@ -0,0 +1,9 @@
+package banking;
+import java.util.*;
+public class bank {
+	public static void main(String[]args) {
+		Scanner sc =new Scanner (System.in);
+		BankAccount ob=new BankAccount("xyz","101");
+		ob.showMenu();
+	}
+}
